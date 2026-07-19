e915f57adbb20077ae5a4808daed1af5ef30f306
MohamadAlahmad <80054797+MohamadAlahmad@users.noreply.github.com>
2026-07-19 11:03:24 +0300

    add firebase crashlytics

--- android/app/src/main/kotlin/com/example/spenza/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/spenza/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.spenza
+package com.spenza.app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,12 +6,16 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import firebase_core
+import firebase_crashlytics
 import flutter_secure_storage_darwin
 import shared_preferences_foundation
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
