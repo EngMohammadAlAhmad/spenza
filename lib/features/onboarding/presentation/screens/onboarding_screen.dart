0ed0529a66e07a6c8adc94a2c8f65d536d1c185d
MohamadAlahmad <80054797+MohamadAlahmad@users.noreply.github.com>
2026-08-03 01:32:52 +0300

    3-8-2026 1:32 AM

--- lib/features/onboarding/presentation/screens/onboarding_screen.dart
+++ lib/features/onboarding/presentation/screens/onboarding_screen.dart
@@ -4,6 +4,7 @@ import 'package:go_router/go_router.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:spenza/core/routes/route_paths.dart';
 import 'package:spenza/core/themes/app_colors.dart';
+import 'package:spenza/core/themes/app_radius.dart';
 import 'package:spenza/core/widgets/custom_button.dart';
 import '../bloc/onboarding_bloc.dart';
 
@@ -59,15 +60,15 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             // Content
             Column(
               children: [
+                const SizedBox(height: 20.0),
                 Expanded(
-                  flex: 6,
+                  flex: 5,
                   child: PageView.builder(
                     controller: _pageController,
                     onPageChanged: (index) => setState(() => _currentIndex = index),
                     itemCount: images.length,
                     itemBuilder: (context, index) {
                       final image = images[index];
-
                       return Image.asset(image);
                     },
                   ),
@@ -75,36 +76,35 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                 Expanded(
                   flex: 2,
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    padding: const .symmetric(horizontal: 24.0),
                     child: Column(
                       children: [
                         const SizedBox(height: 20),
                         SmoothPageIndicator(
                           controller: _pageController,
                           count: images.length,
-                          effect: const ExpandingDotsEffect(
-                            dotHeight: 8,
-                            dotWidth: 8,
+                          effect: ExpandingDotsEffect(
+                            dotHeight: 7.0,
+                            dotWidth: 7.0,
                             activeDotColor: AppColors.primary,
-                            dotColor: Color(0xFFF0D0CB),
+                            dotColor: AppColors.primary.withValues(alpha: 0.3),
                           ),
                         ),
-                        const Spacer(),
+                        SizedBox(height: MediaQuery.sizeOf(context).height * 0.03),
                         if (_currentIndex < images.length - 1) ...[
                           _buildPrimaryButton('التالي', _onNext),
-                          const SizedBox(height: 16),
+                          const SizedBox(height: 10.0),
                           _buildTextButton('تخطي', _complete),
                         ] else ...[
                           _buildPrimaryButton('بلّش تسوّق كزائر', _complete),
                           const SizedBox(height: 12),
                           _buildSecondaryButton('إنشاء حساب جديد', () {
-                            // TODO: Navigate to register
                             _complete();
                           }),
-                          const SizedBox(height: 16.0),
+                          SizedBox(height: MediaQuery.sizeOf(context).height * 0.015),
                           _buildFooterText(),
                         ],
-                        const SizedBox(height: 25.0),
+                        //SizedBox(height: MediaQuery.sizeOf(context).height * 0.02),
                       ],
                     ),
                   ),
@@ -113,7 +113,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             ),
             Positioned(
               right: MediaQuery.sizeOf(context).width * 0.05,
-              top: MediaQuery.sizeOf(context).height * 0.06,
+              top: MediaQuery.sizeOf(context).height * 0.055,
               child: _currentIndex !=0 ? CircleActionButton(
                 icon: Icons.arrow_back_ios_rounded,
                   shareButton: true,
@@ -130,26 +130,31 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   Widget _buildPrimaryButton(String text, VoidCallback onPressed) {
     return SizedBox(
       width: double.infinity,
-      height: 56,
+      height: 48.0,
       child: ElevatedButton(
         onPressed: onPressed,
         style: ElevatedButton.styleFrom(
           backgroundColor: AppColors.primary,
           foregroundColor: Colors.white,
-          elevation: 0,
+          elevation: 0.0,
+          padding: EdgeInsets.zero,
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
+            borderRadius: AppRadius.extra4Large,
           ),
         ),
         child: Row(
-          spacing: 5.0,
           mainAxisAlignment: .center,
           children: [
             Text(
               text,
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, fontFamily: 'Zain'),
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                height: 1.1, // Adjust for font baseline if necessary
+              ),
             ),
-            Icon(Icons.arrow_forward_ios),
+            const SizedBox(width: 8.0),
+            const Icon(Icons.arrow_forward_ios, size: 16),
           ],
         ),
       ),
@@ -159,15 +164,16 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   Widget _buildSecondaryButton(String text, VoidCallback onPressed) {
     return SizedBox(
       width: double.infinity,
-      height: 56,
+      height: 48.0,
       child: OutlinedButton(
         onPressed: onPressed,
         style: OutlinedButton.styleFrom(
           side: const BorderSide(color: AppColors.primary, width: 1.5),
           foregroundColor: AppColors.primary,
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
+            borderRadius: AppRadius.extra4Large,
           ),
+          padding: EdgeInsets.zero,
         ),
         child: Text(
           text,
